55c488b62ec412e9e0be323d8c93f100396610c3
Sarmat Khachirov <skhachirov12@gmail.com>
2020-02-02 09:41:18 +0300

    Add Header & Footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-footer color="blue-grey darken-4">
+    <v-row justify="center" no-gutters>
+      <v-col class="blue-grey darken-4 py-4 text-center white--text" cols="12">
+        <strong>ToDo App — made by Sarmat Enterprise Incorporation Industries Ltd & Co.</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app-bar app color="blue-grey darken-4" dark>
+    <v-toolbar-title>ToDo App</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn>Home</v-btn>
+    <v-btn>Login</v-btn>
+    <v-btn>Register</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
