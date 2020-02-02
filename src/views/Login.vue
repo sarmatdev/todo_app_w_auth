0609a6e009f48f2bc86d9ac48aa309f00cbca6d4
Sarmat Khachirov <skhachirov12@gmail.com>
2020-02-02 09:41:39 +0300

    Add Login page view

---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card width="400" class="mx-auto mt-5">
+    <v-card-title>
+      <h1 class="display-1">Login</h1>
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+        <v-text-field
+          type="password"
+          label="Password"
+          prepend-icon="mdi-lock"
+        />
+      </v-form>
+    </v-card-text>
+    <v-divider></v-divider>
+    <v-card-actions>
+      <v-btn color="success">Register</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn color="info">Login</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
