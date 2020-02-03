1eb8ea1764a83825d7637eba7724d61cc4ea1f82
Sarmat Khachirov <skhachirov12@gmail.com>
2020-02-03 09:17:58 +0300

    Added 404 page

---
+++ src/views/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" style="height: 300px;" align="center" justify="center">
+        <h1>Oops! We can't find this page🤷🏻‍♂️</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
