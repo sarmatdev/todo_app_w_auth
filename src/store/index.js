70849620174f1aa0cab503f7b1825db6707ed129
Sarmat Khachirov <skhachirov12@gmail.com>
2020-02-04 11:49:28 +0300

    Realized module sctructure in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import user from './modules/user/user';
+import common from './modules/common';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    common,
+    user
   }
-})
+});
