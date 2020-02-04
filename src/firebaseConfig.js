0b91be618942d71b16f87f416ea8d22f14c99306
Sarmat Khachirov <skhachirov12@gmail.com>
2020-02-04 11:47:50 +0300

    Firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -15,14 +15,3 @@ const firebaseConfig = {
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export default firebaseApp;
-
-// export default {
-//   apiKey: process.env.VUE_APP_FB_API,
-//   authDomain: 'todo-app-w-auth-b3006.firebaseapp.com',
-//   databaseURL: 'https://todo-app-w-auth-b3006.firebaseio.com',
-//   projectId: 'todo-app-w-auth-b3006',
-//   storageBucket: 'todo-app-w-auth-b3006.appspot.com',
-//   messagingSenderId: '150582566009',
-//   appId: '1:150582566009:web:39c3f55ddeef8dab8bc8a8',
-//   measurementId: 'G-3CQ72M09TT'
-// };
