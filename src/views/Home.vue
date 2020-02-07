268ee4e2520d0f304af2017161a2944e7e992f32
Sarmat Khachirov <skhachirov12@gmail.com>
2020-02-07 18:18:44 +0300

    Add create/get tasks hooks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,12 +8,19 @@
     <v-row>
       <v-col cols="12">
         <!-- ToDo-START -->
+        {{ getTasks }}
         <v-list class="mx-auto">
-          <v-card class="mx-auto mb-5" max-width="500px">
-            <v-card-text class="subtitle-1">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quo
-              modi, quod odit iure vel quisquam fuga laudantium voluptas ipsa
-              aspernatur sit, deserunt numquam!
+          <v-card
+            class="mx-auto mb-5"
+            max-width="500px"
+            v-for="task in getTasks"
+            :key="task.id"
+          >
+            <v-card-text class="title" :class="{ done }">
+              {{ task.title }}
+            </v-card-text>
+            <v-card-text class="title" :class="{ done }">
+              {{ task.description }}
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
@@ -23,64 +30,14 @@
               <v-btn class="mx-2" fab dark small color="error">
                 <v-icon dark>mdi-delete</v-icon>
               </v-btn>
-              <v-btn class="mx-2" fab dark small color="success">
-                <v-icon dark>mdi-check</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-          <v-card class="mx-auto mb-5" max-width="500px">
-            <v-card-text class="subtitle-1">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quo
-              modi, quod odit iure vel quisquam fuga laudantium voluptas ipsa
-              aspernatur sit, deserunt numquam!
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn class="mx-2" fab dark small color="primary">
-                <v-icon dark>mdi-pencil</v-icon>
-              </v-btn>
-              <v-btn class="mx-2" fab dark small color="error">
-                <v-icon dark>mdi-delete</v-icon>
-              </v-btn>
-              <v-btn class="mx-2" fab dark small color="success">
-                <v-icon dark>mdi-check</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-          <v-card class="mx-auto mb-5" max-width="500px">
-            <v-card-text class="subtitle-1">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quo
-              modi, quod odit iure vel quisquam fuga laudantium voluptas ipsa
-              aspernatur sit, deserunt numquam!
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn class="mx-2" fab dark small color="primary">
-                <v-icon dark>mdi-pencil</v-icon>
-              </v-btn>
-              <v-btn class="mx-2" fab dark small color="error">
-                <v-icon dark>mdi-delete</v-icon>
-              </v-btn>
-              <v-btn class="mx-2" fab dark small color="success">
-                <v-icon dark>mdi-check</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-          <v-card class="mx-auto mb-5" max-width="500px">
-            <v-card-text class="subtitle-1">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quo
-              modi, quod odit iure vel quisquam fuga laudantium voluptas ipsa
-              aspernatur sit, deserunt numquam!
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn class="mx-2" fab dark small color="primary">
-                <v-icon dark>mdi-pencil</v-icon>
-              </v-btn>
-              <v-btn class="mx-2" fab dark small color="error">
-                <v-icon dark>mdi-delete</v-icon>
-              </v-btn>
-              <v-btn class="mx-2" fab dark small color="success">
+              <v-btn
+                class="mx-2"
+                fab
+                dark
+                small
+                color="success"
+                @click="checkDone"
+              >
                 <v-icon dark>mdi-check</v-icon>
               </v-btn>
             </v-card-actions>
@@ -99,11 +56,20 @@
             >What You want to do?</v-card-title
           >
           <v-card-text>
-            <v-text-field placeholder="Type task here..."></v-text-field>
+            <v-text-field
+              v-model="task.title"
+              placeholder="Type task here..."
+            ></v-text-field>
+            <v-text-field
+              v-model="task.description"
+              placeholder="Description (optional)"
+            ></v-text-field>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn rounded color="primary" block dark>Add task</v-btn>
+            <v-btn rounded color="primary" block dark @click="saveTask"
+              >Add task</v-btn
+            >
             <v-spacer></v-spacer>
           </v-card-actions>
         </v-card>
@@ -113,7 +79,8 @@
       class="mx-2"
       @click="showInput"
       fixed
-
+      bottom
+      right
       dark
       fab
       large
@@ -122,22 +89,53 @@
       <v-icon dark large>mdi-plus</v-icon>
     </v-btn>
   </v-container>
-  
 </template>
 
 <script>
 export default {
   data() {
     return {
-      dialog: false
+      dialog: false,
+      done: false,
+      task: {
+        title: '',
+        description: ''
+      }
     };
   },
   methods: {
     showInput() {
       this.dialog = !this.dialog;
+    },
+    checkDone() {
+      this.done = !this.done;
+    },
+    saveTask() {
+      this.$store.dispatch('newTask', this.task);
+    }
+  },
+  computed: {
+    getTasks() {
+      return this.$store.getters.getTasks;
+      // return [
+      //   {
+      //     title: 1,
+      //     description: 1,
+      //     id: 1
+      //   },
+      //   {
+      //     title: 2,
+      //     description: 2,
+      //     id: 2
+      //   }
+      // ];
     }
   }
 };
 </script>
 
-<style></style>
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
