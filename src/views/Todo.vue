e2b205f1396ffdf7c958c03ad0cb0459de58c733
Sarmat Khachirov <skhachirov12@gmail.com>
2020-02-25 11:18:17 +0300

    Create Todo view page

---
+++ src/views/Todo.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-row align="center">
+      <v-col cols="12" align="center">
+        <h1 color="blue lighten-2">Todos Page</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <TodosList />
+      </v-col>
+    </v-row>
+    <TodoInput />
+  </v-container>
+</template>
+
+<script>
+import TodosList from '../components/TodosList';
+import TodoInput from '../components/TodoInput';
+export default {
+  components: {
+    TodosList,
+    TodoInput,
+  }
+};
+</script>
+
+<style></style>
