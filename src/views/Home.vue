711bbf9377cc7069d16ef83b70bbe54f625429e4
Sarmat Khachirov <skhachirov12@gmail.com>
2020-02-25 11:19:38 +0300

    Replace Home view into Todo view page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,39 +2,13 @@
   <v-container>
     <v-row align="center">
       <v-col cols="12" align="center">
-        <h1 color="blue lighten-2">Your ToDos</h1>
+        <h1 color="blue lighten-2">Home</h1>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="12">
         <!-- ToDo-START -->
-        <v-list class="mx-auto">
-          <v-card
-            class="mx-auto mb-5"
-            max-width="500px"
-            v-for="task in getTodos"
-            :key="task.id"
-          >
-            <v-card-text class="title" :class="{ done }">
-              {{ task.title }}
-            </v-card-text>
-            <v-card-text class="title" :class="{ done }">
-              {{ task.description }}
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn class="mx-2" fab dark small color="primary">
-                <v-icon dark>mdi-pencil</v-icon>
-              </v-btn>
-              <v-btn class="mx-2" fab dark small color="error">
-                <v-icon dark>mdi-delete</v-icon>
-              </v-btn>
-              <v-btn class="mx-2" fab dark small color="success">
-                <v-icon dark>mdi-check</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-list>
+        
 
         <!-- ToDo-END -->
       </v-col>
@@ -42,7 +16,7 @@
     <!-- Add ToDo button -->
 
     <v-row justify="center">
-      <v-dialog v-model="dialog" max-width="400px">
+      <v-dialog  max-width="400px">
         <v-card>
           <v-card-title class="title font-weight-regular"
             >What You want to do?</v-card-title
@@ -65,13 +39,11 @@
         </v-card>
       </v-dialog>
     </v-row>
-    <v-btn class="mx-2" fixed bottom right dark fab large color="cyan">
-      <v-icon dark large>mdi-plus</v-icon>
-    </v-btn>
   </v-container>
 </template>
 
 <script>
+
 export default {
   data() {
     return {
@@ -83,19 +55,11 @@ export default {
       }
     };
   },
-  created() {
-    this.$store.dispatch('loadTodos');
-  },
-  computed: {
-    getTodos() {
-      return this.$store.getters.getTodos;
-    }
-  }
+  
+  
 };
 </script>
 
 <style>
-.done {
-  text-decoration: line-through;
-}
+
 </style>
