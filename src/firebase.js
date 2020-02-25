1d0920a1cf52ae2e1017a033b250f85db486a1be
Sarmat Khachirov <skhachirov12@gmail.com>
2020-02-25 11:16:09 +0300

    Update firebase config

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const config = {
+  apiKey: process.env.VUE_APP_FB_API,
+  authDomain: 'todo-app-w-auth-b3006.firebaseapp.com',
+  databaseURL: 'https://todo-app-w-auth-b3006.firebaseio.com',
+  projectId: 'todo-app-w-auth-b3006',
+  storageBucket: 'todo-app-w-auth-b3006.appspot.com',
+  messagingSenderId: '150582566009',
+  appId: '1:150582566009:web:39c3f55ddeef8dab8bc8a8',
+  measurementId: 'G-3CQ72M09TT'
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(config);
+
+const firestore = firebaseApp.firestore();
+firestore.settings({});
+export default firestore;
