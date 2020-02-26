d7ccdd380818c392f5d00348e337581822b77d08
Sarmat Khachirov <skhachirov12@gmail.com>
2020-02-26 12:36:24 +0300

    Create snackbar component

---
+++ src/components/UI/Snackbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-snackbar v-model="snackbar" color="error" :timeout="timeout" right top>
+    {{ status }}
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      snackbar: false,
+      timeout: 5000,
+      status: 'Dæ izær xorz!'
+    };
+  }
+};
+</script>
+
+<style></style>
