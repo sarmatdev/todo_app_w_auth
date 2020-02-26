bd15e7ce82cb2e32efb7a1d08b0dfbfd6878edf9
Sarmat Khachirov <skhachirov12@gmail.com>
2020-02-26 12:35:23 +0300

    Realized login/logout/stay logged functionality

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -6,7 +6,8 @@ export default {
   },
   mutations: {
     setUser(state, payload) {
-      console.log(state, payload);
+      state.user = payload;
+      console.log(state.user);
     }
   },
   actions: {
@@ -19,12 +20,42 @@ export default {
             displayName: name
           });
           console.log(res.user);
-          commit('setUser');
+          const user = {
+            name,
+            id: res.user.uid,
+            email: res.user.email
+          };
+          commit('setUser', user);
         })
         .catch(error => {
           console.log(error);
         });
+    },
+    logIn({ commit }, { email, password }) {
+      firebaseApp
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(res => {
+          const user = {
+            name: res.user.displayName,
+            id: res.user.uid,
+            email: res.user.email
+          };
+          commit('setUser', user);
+        })
+        .catch();
+    },
+    logOut({ commit }) {
+      firebaseApp.auth().signOut();
+      commit('setUser', null);
+    },
+    stayLogged({ commit }, user) {
+      commit('setUser', user);
     }
   },
-  getters: {}
+  getters: {
+    checkUser(state) {
+      return state.user !== null;
+    }
+  }
 };
